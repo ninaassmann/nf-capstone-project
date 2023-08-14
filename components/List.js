080588830bc87fbb0e0f5c51d2930d01b6f468eb
Nina Aßmann <hallo@ninaassmann.de>
2023-08-14 13:04:07 +0200

    add age calculate function

--- components/List.js
+++ components/List.js
@@ -1,9 +1,20 @@
 import { styled } from "styled-components";
+import ListItem from "./ListItem";
 
 export default function List({ pets }) {
   return (
     <StyledList>
-      {pets && pets.map((pet) => <li key={pet.id}>{pet.petName}</li>)}
+      {pets &&
+        pets.map((pet) => (
+          <ListItem
+            key={pet.id}
+            name={pet.petName}
+            breed={pet.petBreed}
+            birthday={pet.petBirthday}
+          >
+            {pet.petName} {pet.petBreed}
+          </ListItem>
+        ))}
     </StyledList>
   );
 }
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,13 +1,25 @@
+import { styled } from "styled-components";
+
 export default function ListItem({ id, name, breed, birthday }) {
   const date = new Date();
   const birthdayDate = new Date(birthday);
-  const difference = date.getTime() - birthday.getTime();
+  const difference = date.getTime() - birthdayDate.getTime();
+  let months = Math.floor(difference / 2629746000);
+  const years = Math.floor(months / 12);
+  months = months - 12 * years;
+  const age = `${years}.${months} years`;
 
   return (
-    <li key={id}>
+    <StyledListItem>
       <h3>{name}</h3>
       <p>{breed}</p>
-      <p>{difference}</p>
-    </li>
+      {years > 1 ? <p>{age}</p> : <p>{months} months</p>}
+    </StyledListItem>
   );
 }
+
+const StyledListItem = styled.li`
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: lightgray;
+`;
