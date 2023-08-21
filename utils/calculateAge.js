0caa92d8c84fd8116d365b6b84ed5d70aee6c26f
Nina Aßmann <hallo@ninaassmann.de>
2023-08-21 12:26:34 +0200

    update age calculator to the days

--- utils/calculateAge.js
+++ utils/calculateAge.js
@@ -2,11 +2,16 @@ export function calculateAge(birthday) {
   const date = new Date();
   const birthdayDate = new Date(birthday);
   const difference = date.getTime() - birthdayDate.getTime();
+  const days = Math.floor(difference / 86400000);
+
   let months = Math.floor(difference / 2629746000);
   const years = Math.floor(months / 12);
-  months = months - 12 * years;
-  const age = `${years}.${months} years`;
 
-  if (years > 1) return age;
-  return `${months} months`;
+  const ageInYears = `${years} years`;
+  const ageInMonths = `${months - 12 * years} months`;
+  const ageInDays = `${days} days`;
+
+  if (years > 1) return ageInYears;
+  else if (years < 1 && months < 1) return ageInDays;
+  return ageInMonths;
 }
