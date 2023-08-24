0aa8f18eb8d256093e80aa688bafdcdf782f3d69
Nina Aßmann <hallo@ninaassmann.de>
2023-08-24 12:19:07 +0200

    add thumbnail to list and basic styling

--- pages/breeds/index.js
+++ pages/breeds/index.js
@@ -1,15 +1,49 @@
+import Container from "@/components/Container.styled";
+import Thumbnail from "@/components/Thumbnail";
 import Link from "next/link";
-import useSWR from "swr";
+import { styled } from "styled-components";
 
 export default function BreedList({ dogData }) {
   return (
-    <ul>
-      {dogData &&
-        dogData.map((breed) => (
-          <li key={breed.id}>
-            <Link href={`/breeds/${breed.slug}`}>{breed.name}</Link>
-          </li>
-        ))}
-    </ul>
+    <main>
+      <Container>
+        <List>
+          {dogData &&
+            dogData.map((breed) => (
+              <li key={breed.id}>
+                <StyledLink href={`/breeds/${breed.slug}`}>
+                  <Thumbnail breed={breed} />
+                  <h3>{breed.name}</h3>
+                </StyledLink>
+              </li>
+            ))}
+        </List>
+      </Container>
+    </main>
   );
 }
+
+const List = styled.ul`
+  width: 100%;
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  list-style: none;
+`;
+
+const StyledLink = styled(Link)`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: lightgray;
+  color: black;
+  text-decoration: none;
+
+  &:hover {
+    filter: brightness(0.85);
+  }
+`;
