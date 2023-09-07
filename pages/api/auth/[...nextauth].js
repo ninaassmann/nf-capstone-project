a69be51f84c15cd6b7b572de281f982c8f7a7efe
Nina Aßmann <hallo@ninaassmann.de>
2023-09-07 14:56:11 +0200

    add google login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,30 +4,7 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./lib/mongodb";
 
 import GithubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
-
-const fakeLogin = CredentialsProvider({
-  name: "Credentials",
-  credentials: {
-    username: { label: "Username", type: "text", placeholder: "tester" },
-    password: { label: "Password", type: "password" },
-  },
-  // and adding a fake authorization with static username and password:
-  async authorize(credentials) {
-    if (
-      credentials.username === "tester" &&
-      credentials.password === "tester1234!"
-    ) {
-      return {
-        id: "1",
-        name: "Tester",
-        email: "tester@github",
-      };
-    } else {
-      return null;
-    }
-  },
-});
+import GoogleProvider from "next-auth/providers/google";
 
 const providers =
   process.env.VERCEL_ENV === "preview"
@@ -36,6 +13,10 @@ const providers =
         GithubProvider({
           clientId: process.env.GITHUB_ID,
           clientSecret: process.env.GITHUB_SECRET,
+        }),
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
         // ...add more providers here
       ];
