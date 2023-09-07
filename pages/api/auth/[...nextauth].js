0d8773e1c481156679a01704b0b505767f5cd56e
Nina Aßmann <hallo@ninaassmann.de>
2023-09-07 15:15:20 +0200

    add fake login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,30 @@ import clientPromise from "./lib/mongodb";
 
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const fakeLogin = CredentialsProvider({
+  name: "Credentials",
+  credentials: {
+    username: { label: "Username", type: "text", placeholder: "tester" },
+    password: { label: "Password", type: "password" },
+  },
+  // and adding a fake authorization with static username and password:
+  async authorize(credentials) {
+    if (
+      credentials.username === "tester" &&
+      credentials.password === "tester1234!"
+    ) {
+      return {
+        id: "1",
+        name: "Tester",
+        email: "tester@github",
+      };
+    } else {
+      return null;
+    }
+  },
+});
 
 const providers =
   process.env.VERCEL_ENV === "preview"
