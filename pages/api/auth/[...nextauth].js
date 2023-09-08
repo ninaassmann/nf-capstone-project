6e608ee6fd6dc5cd858c7b23477c87234dc54363
Nina Aßmann <hallo@ninaassmann.de>
2023-09-08 08:44:02 +0200

    delete fake login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,6 @@ import clientPromise from "./lib/mongodb";
 
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 const providers = [
   GithubProvider({
@@ -15,28 +14,6 @@ const providers = [
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  }),
-  CredentialsProvider({
-    name: "Credentials",
-    credentials: {
-      username: { label: "Username", type: "text", placeholder: "tester" },
-      password: { label: "Password", type: "password" },
-    },
-    // and adding a fake authorization with static username and password:
-    authorize(credentials) {
-      if (
-        credentials.username === "tester" &&
-        credentials.password === "tester1234!"
-      ) {
-        return {
-          id: "1",
-          name: "Tester",
-          email: "tester@github",
-        };
-      } else {
-        return null;
-      }
-    },
   }),
   // ...add more providers here
 ];
