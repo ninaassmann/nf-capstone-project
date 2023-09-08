eec0daae7acd70105cc6a5a662a44ca93a62e57c
Nina Aßmann <hallo@ninaassmann.de>
2023-09-08 15:51:50 +0200

    comment mongoDB adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "../../../lib/mongodb";
+/* import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb"; */
 
 import GoogleProvider from "next-auth/providers/google";
 
@@ -14,7 +14,7 @@ const providers = [
 ];
 
 export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise),
+  /* adapter: MongoDBAdapter(clientPromise), */
   providers,
 };
 export default NextAuth(authOptions);
