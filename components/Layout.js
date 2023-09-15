2c4b26178cabd75f86d257b444198772e534b912
Nina Aßmann <97239098+ninaassmann@users.noreply.github.com>
2023-09-15 08:49:34 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -75,6 +75,7 @@ export default function Layout({ children, theme, toggleTheme }) {
 const StyledContainer = styled(Container)`
   height: 100vh;
   width: min(100vw, 800px);
+  margin-inline: auto;
   background-image: linear-gradient(
       to bottom,
       rgba(0, 0, 50, 0.15),
@@ -86,7 +87,8 @@ const StyledContainer = styled(Container)`
 `;
 
 const StyledWrapper = styled.div`
-  width: calc(100% - 4rem);
+  width: min(calc(100vw - 4rem), 736px);
+  margin-inline: auto;
   position: absolute;
   bottom: 10rem;
   left: 2rem;
